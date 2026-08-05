456a1d9b329db8d2519b0d081e0becbc8c3b1498
13865-commits <13865@holbertonstudents.com>
2026-08-05 18:25:06 +0000

    Update 0-async_generator.py with strict PEP8 and type hints

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+"""Async Generator module."""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Loops 10 times, asynchronously waits 1 second each time,
     and yields a random number between 0 and 10.
