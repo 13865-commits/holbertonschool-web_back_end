4fbb71f263092bb3b9b609642c21da58217e59f3
13865-commits <13865@holbertonstudents.com>
2026-08-05 18:20:45 +0000

    Fix python_async_comprehension task 0 with strict PEP8 and init file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for async_generator coroutine.
+Async generator module.
 """
 import asyncio
 import random
@@ -9,7 +9,7 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Coroutine that loops 10 times, asynchronously waits 1 second,
+    Loops 10 times, asynchronously waits 1 second each time,
     and yields a random number between 0 and 10.
     """
     for _ in range(10):
